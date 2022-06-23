c491333e8af1d7673aa6f0eb55a7c210e8de9735
nicholastonelli <nicholasevantonelli@gmail.com>
2022-06-23 17:02:27 -0400

    markedcircle getting closer

--- app/javascript/controllers/clockworks_controller.js
+++ app/javascript/controllers/clockworks_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
 
   makeClock(r = 50, cx = 55, cy = 55) {
     let clockFace = this.createSvg("circle")
-    clockFace.setAttribute("fill", "#DDD")
+    clockFace.setAttribute("fill", "#DDE")
     clockFace.setAttribute("stroke", "black")
     clockFace.setAttribute("stroke-width", "4")
     clockFace.setAttribute("r", r)
@@ -23,11 +23,27 @@ export default class extends Controller {
     clockFace.setAttribute("cy", cy)
     clockFace.setAttribute("x", "55")
     clockFace.setAttribute("y", "55")
-
+    let marked = this.makeMarked(this.markedValue, cx, cy, (r/2.13))
     let sections = this.makeSections(this.sectionsValue, cx, cy, r)
     
     this.svgTarget.appendChild(clockFace)
+    this.svgTarget.appendChild(marked)
     this.svgTarget.appendChild(sections)
+  }
+
+  makeMarked(marked, cx= 55, cy = 55, r = 55){
+    console.log("making marks")
+    let clockMarked = this.createSvg("circle")
+    clockMarked.setAttribute("fill", "transparent")
+    clockMarked.setAttribute("r", r)
+    clockMarked.setAttribute("cx", cx)
+    clockMarked.setAttribute("cy", cy)
+    clockMarked.setAttribute("stroke", "blue")
+    clockMarked.setAttribute("stroke-width", "50")
+    clockMarked.setAttribute("stroke-dasharray", `calc(${this.markedValue * 300} * ${this.sectionsValue} / 100) 157.1`)
+    clockMarked.setAttribute("transform", "rotate(-90) translate(-110)")
+
+    return clockMarked
   }
 
   makeSections(sections, cx, cy, r){
@@ -37,14 +53,10 @@ export default class extends Controller {
             clockLine.setAttribute("stroke", "black") 
             clockLine.setAttribute("stroke-width", "4") 
             clockLine.setAttribute("stroke-width", "4")
-            clockLine.setAttribute("d", `M${cx} ${cy} ${cx + r * Math.cos((2 * Math.PI * i) / sections)} ${cy + r * Math.sin((2 * Math.PI * i) / sections)}`)
+            clockLine.setAttribute("d", `M${cx} ${cy} ${cx + r * Math.sin((2 * Math.PI * i) / sections)} ${cy + r * (-1 * Math.cos((2 * Math.PI * i) / sections))}`)
             clockLines.appendChild(clockLine)
         }
     return clockLines
-  }
-
-  setMarked(){
-
   }
 
   connect() {
