a9380411afe30ee7f8bcc825315ca75f83743a2b
nicholastonelli <nicholasevantonelli@gmail.com>
2022-09-15 15:34:25 -0400

    d3 implemented

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,21 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
-//import { useClickOutside } from 'stimulus-use'
 
 export default class extends Controller {
-  static targets = ["menu", "wrapper"]
+  static targets = ["menu", "button"]
 
-  change() {
-    console.log(`change triggering`)
-    if (this.menuTarget.classList.contains("hidden")) {
+  change(event) {
+
+    /* if (this.menuTarget.classList.contains("hidden")) {
       this.menuTarget.classList.remove("hidden")
     } else {
+      this.menuTarget.classList.add("hidden")
+    } */
+  } 
+
+  outsideClick(event) {
+
+    //console.log(`event.target:`, event.target)
+    //console.log(`this.element:`, this.element)
+    //console.log(`this.menuTarget`, this.menuTarget)
+    //console.log(this.element === event.target)
+    //console.log(this.element.contains(event.target))
+
+    // Ignore event if clicked within element
+    if (
+      this.element === event.target ||
+      this.element.contains(event.target)
+    ) {
+      return
+    } else {
+      // Execute the actual action we're interested in
+      console.log('hidden outside click activating')
       this.menuTarget.classList.add("hidden")
     }
   }
 
-
-
   connect() {
-    this.menuTarget = this.menuTarget.bind(this)
   }
 }
